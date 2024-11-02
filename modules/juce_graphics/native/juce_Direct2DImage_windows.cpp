ca29f3c3c29b92681fd7cc20b336fcc745f30988
ancientjpeg <JwyattK@gmail.com>
2024-11-01 17:48:15 -0700

    fully disable D2D

--- modules/juce_graphics/native/juce_Direct2DImage_windows.cpp
+++ modules/juce_graphics/native/juce_Direct2DImage_windows.cpp
@@ -651,6 +651,7 @@ auto Direct2DPixelData::getPagesForContext (ComSmartPtr<ID2D1DeviceContext1> con
 }
 
 //==============================================================================
+/*
 ImagePixelData::Ptr NativeImageType::create (Image::PixelFormat format, int width, int height, bool clearImage) const
 {
     SharedResourcePointer<DirectX> directX;
@@ -667,6 +668,7 @@ ImagePixelData::Ptr NativeImageType::create (Image::PixelFormat format, int widt
 
     return new Direct2DPixelData (format, width, height, clearImage);
 }
+*/
 
 //==============================================================================
 //==============================================================================
